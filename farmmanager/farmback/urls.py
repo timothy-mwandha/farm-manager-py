cd366b4b9fc812ae0c52fcc4bfb61f7639c84786
steven <kawooyastevenug@gmail.com>
2020-03-18 16:06:04 +0300

    Add routers

--- farmmanager/farmback/urls.py
+++ farmmanager/farmback/urls.py
@@ -1,7 +1,17 @@
 from rest_framework import routers
-from .api import IncomeViewset
+from .api import *
 
 router = routers.DefaultRouter()
-router.register('api/farmback', IncomeViewset, 'farmback')
+router.register('api/user', UserViewset, 'farmback')
+router.register('api/personnel', PersonnelViewset, 'farmback')
+router.register('api/income', IncomeViewset, 'farmback')
+router.register('api/expenditure', ExpenditureViewset, 'farmback')
+router.register('api/consumable', ConsumableViewset, 'farmback')
+router.register('api/harvest', HarvestViewset, 'farmback')
+router.register('api/advance', AdvanceFormViewset, 'farmback')
+router.register('api/payroll', PayrollViewset, 'farmback')
+router.register('api/requisition', RequisitionViewset, 'farmback')
+router.register('api/toolbincard', ToolBinCardViewset, 'farmback')
+
 
 urlpatterns = router.urls
